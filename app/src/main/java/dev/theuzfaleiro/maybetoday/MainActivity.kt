0353cd0638d4d2ea479cdbb2b43a1a4626d13ee7
Matheus Faleiro <theuzfaleiro@gmail.com>
2021-10-19 10:04:45 +0200

    [PROJECT] Add Project  Dependencies

--- app/src/main/java/dev/theuzfaleiro/maybetoday/MainActivity.kt
+++ app/src/main/java/dev/theuzfaleiro/maybetoday/MainActivity.kt
@@ -1,7 +1,7 @@
 package dev.theuzfaleiro.maybetoday
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
